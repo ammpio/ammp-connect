781689b6e83414a76c21b4d84a03935332d4aea7
Svet Bajlekov <tomatoman@gmail.com>
2018-09-22 18:24:41 +0200

    Switch to AutoSSH

--- sshtunnel/bin/tunnel.sh
+++ sshtunnel/bin/tunnel.sh
@@ -3,10 +3,4 @@
 # Load configuration options
 . "$SNAP"/config
 
-while true; do
-
-  "$SNAP"/usr/bin/ssh -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" "$USER"@"$REMOTE"
-
-  # Wait a little if process exits
-  sleep 1m
-done
+"$SNAP"/usr/bin/autossh -M 0 -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" "$USER"@"$REMOTE"
