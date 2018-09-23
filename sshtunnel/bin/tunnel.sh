77bae8739a8ba71fc7643d5727ba201b32105af0
Svet Bajlekov <tomatoman@gmail.com>
2018-09-23 19:43:18 +0200

    Point to actual autossh binary

--- sshtunnel/bin/tunnel.sh
+++ sshtunnel/bin/tunnel.sh
@@ -3,4 +3,4 @@
 # Load configuration options
 . "$SNAP"/config
 
-"$SNAP"/usr/bin/autossh -M 0 -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" -vv "$USER"@"$REMOTE"
+"$SNAP"/usr/lib/autossh/autossh -M 0 -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" -vv "$USER"@"$REMOTE"
