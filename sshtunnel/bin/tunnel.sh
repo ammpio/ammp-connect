d78739ea6bd24e19a835a47bbcfd877303fc4efa
Svet Bajlekov <tomatoman@gmail.com>
2018-09-23 21:38:07 +0200

    Move AutoSSH parameters to environment variables in tunnel.sh

--- sshtunnel/bin/tunnel.sh
+++ sshtunnel/bin/tunnel.sh
@@ -3,4 +3,9 @@
 # Load configuration options
 . "$SNAP"/config
 
-"$SNAP"/usr/lib/autossh/autossh -M 0 -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" -vv "$USER"@"$REMOTE"
+# Set AutoSSH parameters
+export AUTOSSH_PATH="$SNAP"/usr/bin/ssh
+export AUTOSSH_GATETIME=0
+export AUTOSSH_DEBUG=1
+
+"$SNAP"/usr/lib/autossh/autossh -M 0 -o ExitOnForwardFailure=yes -o ConnectTimeout=60 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile="$KNOWN_HOSTS" -p "$PORT" -N -R 0:localhost:22 -i "$PRIV_KEY" -v "$USER"@"$REMOTE"
